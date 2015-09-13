8171c716c7052ed829b3786c81502a58b30f95ce
Oscar Ramirez O <Oscar.Ramirez@DynamicDevStudio.com>
2015-09-13 12:46:11 -0600

    Módulo para seleccionar la cuenta contable según la forma de pago en la elaboración de factura de cliente

---
+++ sbg_accounting_payment_method/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Accounting Payment Method - SBG',
+    'version': '1.0.0',
+    'author': 'Dynamic Development Studio',
+    'summary': 'Account selection for payment methods in sales',
+    'description' : """
+Guns And Ammo Management
+========================
+    """,
+    'website': 'http://www.DynamicDevStudio.com',
+    'category': 'Accounting & Finance',
+    'depends': ["account"],
+    'data': [
+        'view/sbg_account_invoice_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
