ae79baeb193c6b8c2e09ba9908dbeefeb6333ad2
miguelchuga <miguelchuga@gmail.com>
2016-07-14 20:35:26 -0600

    Aactualiza modulos y orden en localgt

--- conciliacion_bancaria/report/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-import banco_reporte
-import banco_reporte_asistente
--- importaciones/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-import purchase
-#import res_company
-#import product
-import polizas
-import catalogos
-import gastos
-import reportes
