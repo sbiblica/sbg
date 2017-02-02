cde63d2d935a4ee7e249a25c29c9301a14a3eb3a
Oscar Ramirez O <oscar.ramirez@acentonet.com>
2017-02-01 23:04:04 -0600

    Carga inicial de módulo de estadísticas

---
+++ sbg_statistics/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Statistical Reports for SBG",
+
+    'summary': """
+        Statistical reports for Bible Society of Guatemala
+    """,
+
+    'description': """
+        Custom statistical reports for Bible Society of Guatemala.
+    """,
+
+    'author': "Dynamic Development Studios",
+    'website': "http://www.DynamicDevStudios.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale', 'point_of_sale', 'rep_ventas'],
+
+    # always loaded
+    'data': [
+        'views/sbg_sales_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ sbg_statistics/models/sbg_sales.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from openerp import tools
+from openerp.osv import fields, osv
+
+class sbg_sales(osv.osv):
+    _name = 'sbg.sales'
+    _description = 'SBG Sales'
+    _auto = False
+
+    _columns = {
+        'reference': fields.char('Reference'),
+        'date_order': fields.datetime(string="Order date"),
+        'partner_id': fields.many2one('res.partner', 'Customer' , readonly=True),
+        'customer_city': fields.char('Customer city'),
+        'customer_type': fields.char('Customer type'),
+        'promoter': fields.char('Promoter'),
+        'location_id': fields.many2one('stock.location', 'Location' , readonly=True),
+        'product_id': fields.many2one('product.product', 'Product' , readonly=True),
+        'qty': fields.float(string='Quantity'),
+        'price_unit': fields.float(string='Unit price'),
+        'line_amount': fields.float(string='Line amount')
+    }
+    _order = 'date_order desc'
+
+    def init(self, cr, uid=1):
+        tools.drop_view_if_exists(cr, self._table)
+        cr.execute("""
+            CREATE OR REPLACE VIEW %s AS (
+                SELECT ol.id, o.name as reference, o.date_order, o.partner_id, COALESCE(p.city, '') as customer_city, COALESCE(p.clase_de_cliente, '') as customer_type,
+                COALESCE(p.promotor, '') as promoter, w.view_location_id as location_id,
+                ol.product_id, COALESCE(ol.product_uom_qty, 0) AS qty, COALESCE(ol.price_unit, 0) AS price_unit,
+                COALESCE(ol.product_uom_qty, 0) * COALESCE(ol.price_unit, 0) - COALESCE(ol.discount, 0) AS line_amount
+                FROM sale_order o
+                LEFT JOIN res_partner p
+                ON p.id = o.partner_id
+                LEFT JOIN stock_warehouse w
+                ON w.id = o.warehouse_id
+                LEFT JOIN sale_order_line ol
+                ON ol.order_id = o.id
+                WHERE o.state not in ('draft', 'cancel')
+                UNION
+                SELECT ol.id, o.name as reference, o.date_order, o.partner_id, COALESCE(p.city, '') as customer_city, COALESCE(p.clase_de_cliente, '') as customer_type,
+                COALESCE(p.promotor, '') as promoter, o.location_id,
+                ol.product_id, COALESCE(ol.qty, 0) AS qty, COALESCE(ol.price_unit, 0) AS price_unit,
+                COALESCE(ol.qty, 0) * COALESCE(ol.price_unit, 0) - COALESCE(ol.discount, 0) AS line_amount
+                FROM pos_order o
+                LEFT JOIN res_partner p
+                ON p.id = o.partner_id
+                LEFT JOIN pos_order_line ol
+                ON ol.order_id = o.id
+                WHERE o.state in ('invoiced')
+            )
+        """ % (self._table))
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
